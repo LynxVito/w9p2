e2ce349d8788197c2122723e262d644736e3860c
LynxVito <mzb508@york.ac.uk>
2025-04-25 14:25:32 +0100

    started adding code to p5js

--- stuff.js
+++ stuff.js
@@ -1,9 +1,15 @@
 function setup() {
     createCanvas(800, 600);
-  }
+}
   
-  function draw() {
-    background(100, 80, 150);
+function draw() {
+  background(100, 80, 150);
 
-    
+  fill(255);
+  circle(mouseX, mouseY, 80);
+
+  if(mouseIsPressed === true) {
+    fill(0);
+    circle(mouseX, mouseY, 80);
   }
+}
